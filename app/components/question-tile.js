0800c755d4403e2902d4838b444d0041c3255dee
Josue Valverde <josuevalverde12@gmail.com>
2015-10-06 22:15:14 -0700

    add functionality to add a new question and display all questions in database.

---
+++ app/components/question-tile.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  questionForm: false,
+
+  actions: {
+    questionFormShow() {
+      this.set('questionForm', true);
+    },
+
+    questionFormHide() {
+      this.set('questionForm', false);
+    },
+
+    saveQuestion() {
+      var params = [];
+      params = {
+        title: this.get('title'),
+        author: this.get('author')
+      }
+      this.set('questionForm', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
