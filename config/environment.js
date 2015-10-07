9407fddef1414f23aafb76c76c282ea90ad61fd2
Josue Valverde <josuevalverde12@gmail.com>
2015-10-06 20:16:27 -0700

    create initial files.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'question-answer-message-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://q-and-a-board.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
